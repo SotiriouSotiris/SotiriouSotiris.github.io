c24d67dd3d16e6ca2607cf399dec3c1707f75af5
SotirisSotiriou09 <92454226+SotirisSotiriou09@users.noreply.github.com>
2022-02-06 15:27:56 +0000

    more animations
    scroll triggered animations

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline({ defaults: {ease: "power4.inOut", duration : 0.5}});
 
 if(window.innerWidth>=800){
@@ -9,3 +10,47 @@ if(window.innerWidth>=800){
     .from( '#watermark', { x : "100%", duration: 2}, "-=2")
     .from( '.socialIcons', {opacity: 0, duration: 2, stagger: 0.2, x: "-200px"}, "-=3");
 }
+
+gsap.from( ".scrollAnim", {
+    scrollTrigger: {
+        trigger: '.scrollAnim',
+        start: "top center"
+    },
+    y: 100,
+    opacity: 0,
+    ease: "none",
+    duration: 0.5
+} );
+
+gsap.from( "#aboutSelf", {
+    scrollTrigger:{
+        trigger: '#aboutSelf',
+        start: "top 80%"
+    },
+    x: -100,
+    opacity: 0,
+    ease: "none",
+    duration: 0.5
+} );
+
+gsap.from( "#aboutCom", {
+    scrollTrigger:{
+        trigger: '#aboutCom',
+        start: "top 80%"
+    },
+    x: 100,
+    opacity: 0,
+    ease: "none",
+    duration: 0.5
+} );
+
+gsap.from( "#forth-part", {
+    scrollTrigger: {
+        trigger: '#forth-part',
+        start: "top center"
+    },
+    y: 100,
+    opacity: 0,
+    ease: "none",
+    duration: 0.5
+} );
