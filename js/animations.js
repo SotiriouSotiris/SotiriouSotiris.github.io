9c7359bd13ee526cec16154d95e50ca646129820
SotirisSotiriou09 <92454226+SotirisSotiriou09@users.noreply.github.com>
2022-02-08 11:46:10 +0000

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -11,46 +11,46 @@ if(window.innerWidth>=800){
     .from( '.socialIcons', {opacity: 0, duration: 2, stagger: 0.2, x: "-200px"}, "-=3");
 }
 
-gsap.from( ".scrollAnim", {
-    scrollTrigger: {
-        trigger: '.scrollAnim',
-        start: "top center"
-    },
-    y: 100,
-    opacity: 0,
-    ease: "none",
-    duration: 0.5
-} );
+// gsap.from( ".scrollAnim", {
+//     scrollTrigger: {
+//         trigger: '.scrollAnim',
+//         start: "top center"
+//     },
+//     y: 100,
+//     opacity: 0,
+//     ease: "none",
+//     duration: 0.5
+// } );
 
-gsap.from( "#aboutSelf", {
-    scrollTrigger:{
-        trigger: '#aboutSelf',
-        start: "top 80%"
-    },
-    x: -100,
-    opacity: 0,
-    ease: "none",
-    duration: 0.5
-} );
+// gsap.from( "#aboutSelf", {
+//     scrollTrigger:{
+//         trigger: '#aboutSelf',
+//         start: "top 80%"
+//     },
+//     x: -100,
+//     opacity: 0,
+//     ease: "none",
+//     duration: 0.5
+// } );
 
-gsap.from( "#aboutCom", {
-    scrollTrigger:{
-        trigger: '#aboutCom',
-        start: "top 80%"
-    },
-    x: 100,
-    opacity: 0,
-    ease: "none",
-    duration: 0.5
-} );
+// gsap.from( "#aboutCom", {
+//     scrollTrigger:{
+//         trigger: '#aboutCom',
+//         start: "top 80%"
+//     },
+//     x: 100,
+//     opacity: 0,
+//     ease: "none",
+//     duration: 0.5
+// } );
 
-gsap.from( "#forth-part", {
-    scrollTrigger: {
-        trigger: '#forth-part',
-        start: "top center"
-    },
-    y: 100,
-    opacity: 0,
-    ease: "none",
-    duration: 0.5
-} );
+// gsap.from( "#forth-part", {
+//     scrollTrigger: {
+//         trigger: '#forth-part',
+//         start: "top center"
+//     },
+//     y: 100,
+//     opacity: 0,
+//     ease: "none",
+//     duration: 0.5
+// } );
